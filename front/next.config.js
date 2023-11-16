ec31991ef0e6a08be3805661bd3c47c2e6ce752c
brgndy <brgndy96@gmail.com>
2023-11-16 10:26:22 +0900

    chore : vanilla-extract 설치

--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,8 @@
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(wav)$/,
@@ -23,4 +23,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = withVanillaExtract(nextConfig);
