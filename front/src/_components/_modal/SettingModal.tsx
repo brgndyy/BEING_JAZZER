c488e81ea647727646c0814d89c955299509f8ce
brgndy <brgndy96@gmail.com>
2023-11-19 14:10:09 +0900

    feat(modal): 모달 컴포넌트 바인딩

---
+++ front/src/_components/_modal/SettingModal.tsx
@@ -0,0 +1,3 @@
+export default function SettingModal() {
+  return <div>SettingModal</div>;
+}
--- front/src/_components/_setting/Setting.tsx
+++ front/src/_components/_setting/Setting.tsx
@@ -1,11 +1,26 @@
 import { IoSettingsSharp } from 'react-icons/io5';
 import { settingIcon } from './setting.css';
 import { myStyle } from '@/_styles/vars.css';
+import { createPortal } from 'react-dom';
+import useModal from '@/_hooks/useModal';
+import SettingModal from '../_modal/SettingModal';
+import Modal from '../_modal/Modal';
 
 export default function Setting() {
+  let { modal, modalHandler, portalElement, closing, handleClose } = useModal();
+
   return (
     <>
-      <IoSettingsSharp className={`${settingIcon} ${myStyle}`} />
+      <IoSettingsSharp className={`${settingIcon} ${myStyle}`} onClick={modalHandler} />
+
+      {modal && portalElement
+        ? createPortal(
+            <Modal modalHandler={modalHandler} closing={closing} handleClose={handleClose}>
+              <SettingModal />
+            </Modal>,
+            portalElement,
+          )
+        : null}
     </>
   );
 }
