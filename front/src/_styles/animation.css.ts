85e47c32330f1408e0b5ead8e8e2035d3dc69aca
brgndy <brgndy96@gmail.com>
2023-11-19 14:10:30 +0900

    style: 모달 마운트, 언마운트시 애니메이션 설정

--- front/src/_styles/animation.css.ts
+++ front/src/_styles/animation.css.ts
@@ -8,3 +8,33 @@ export const softAppear = keyframes({
     opacity: 1,
   },
 });
+
+export const fadeIn = keyframes({
+  '0%': {
+    opacity: '0',
+    transform: 'translateY(400px) scale(0.75)',
+  },
+  '75%': {
+    opacity: '1',
+    transform: 'translateY(-16px) scale(1)',
+  },
+  '100%': {
+    opacity: '1',
+    transform: 'translateY(0px)',
+  },
+});
+
+export const fadeOut = keyframes({
+  '0%': {
+    opacity: '1',
+    transform: 'translateY(0px)',
+  },
+  '75%': {
+    opacity: '1',
+    transform: 'translateY(-16px) scale(1)',
+  },
+  '100%': {
+    opacity: '0',
+    transform: 'translateY(400px) scale(0.75)',
+  },
+});
