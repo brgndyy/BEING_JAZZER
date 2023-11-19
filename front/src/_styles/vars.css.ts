8cd11ea7727d216e4034b2d800bb5d62f03eab85
brgndy <brgndy96@gmail.com>
2023-11-19 13:08:51 +0900

    style : text1 스타일변수 선언

--- front/src/_styles/vars.css.ts
+++ front/src/_styles/vars.css.ts
@@ -1,15 +1,18 @@
 import { createVar, style } from '@vanilla-extract/css';
 
 export const headerBg = createVar();
+export const text1 = createVar();
 
 export const myStyle = style({
   vars: {
     [headerBg]: '#fff',
+    [text1]: '#000',
   },
   selectors: {
     '[data-theme="dark"] &': {
       vars: {
-        [headerBg]: '#000',
+        [headerBg]: '#1e1e1e',
+        [text1]: '#fff',
       },
     },
   },
