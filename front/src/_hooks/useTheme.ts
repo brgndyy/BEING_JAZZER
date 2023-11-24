24370704d41915c90d71dc428cd951f5d8cbaee0
brgndy <brgndy96@gmail.com>
2023-11-24 16:32:20 +0900

    Feat(theme): 테마 전역으로 관리

--- front/src/_hooks/useTheme.ts
+++ front/src/_hooks/useTheme.ts
@@ -1,7 +1,14 @@
 import { useEffect, useState } from 'react';
+import { useSetAtom } from 'jotai';
+import { themeAtom } from '@/_store/themeAtom';
 
 export const useTheme = (currentTheme: string) => {
   const [darkTheme, setDarkTheme] = useState<boolean>(currentTheme === 'dark');
+  const setDarkMode = useSetAtom(themeAtom);
+
+  useEffect(() => {
+    setDarkMode(darkTheme);
+  }, [darkTheme, setDarkMode]);
 
   const themeToggleHandler = async () => {
     const newTheme = !darkTheme;
