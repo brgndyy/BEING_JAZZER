9c8a86e434ac96f5664e5085b72c693de93ad740
brgndy <brgndy96@gmail.com>
2023-11-24 15:39:49 +0900

    Remove: cors 옵션 관련 파일 삭제

--- backend/src/constants/corsOption.ts
+++
@@ -1,9 +0,0 @@
-import deepFreeze from '../utils/deepFreeze';
-
-const corsOptions = deepFreeze({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-});
-
-export default corsOptions;
