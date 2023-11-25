6741625046fc1eec52cdf799a4bd772f6d0ccae0
brgndy <brgndy96@gmail.com>
2023-11-25 18:45:04 +0900

    feat(constants): 라우트 경로 상수 추가

--- front/src/_constants/pathRoutes.ts
+++ front/src/_constants/pathRoutes.ts
@@ -2,6 +2,7 @@ import deepFreeze from '@/_utils/deepFreeze';
 
 const PATH_ROUTES = deepFreeze({
   request_auth_email: '/api/users/sendAuthEmail',
+  register_user_info: '/api/users/register',
 });
 
 export default PATH_ROUTES;
