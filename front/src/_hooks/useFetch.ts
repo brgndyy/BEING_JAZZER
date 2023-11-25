8a0afc1eca6767674f277239fc712fe3349d367f
brgndy <brgndy96@gmail.com>
2023-11-25 14:19:28 +0900

    feat(custom hook): useFetch 커스텀 훅 선언

---
+++ front/src/_hooks/useFetch.ts
@@ -0,0 +1,62 @@
+import { useState, useCallback, useEffect, useRef } from 'react';
+
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>();
+  const activeHttpRequests = useRef<AbortController[]>([]);
+
+  const sendRequest = useCallback(
+    async <T>(
+      url: string,
+      body: BodyInit | null = null,
+      headers: Record<string, string> = {},
+      method: string = 'GET',
+    ): Promise<T> => {
+      setIsLoading(true);
+
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const res = await fetch(url, {
+          method,
+          body,
+          credentials: 'include',
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const data = await res.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl,
+        );
+
+        setIsLoading(false);
+        return data;
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        }
+        setIsLoading(false);
+
+        throw err;
+      }
+    },
+    [],
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError, setError };
+};
+
+export default useFetch;
