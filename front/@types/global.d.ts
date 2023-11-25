9033e63ec670c3b67721700668e7ab52b284164f
brgndy <brgndy96@gmail.com>
2023-11-25 14:20:00 +0900

    feat(type): 타입선언

--- front/@types/global.d.ts
+++ front/@types/global.d.ts
@@ -20,11 +20,53 @@ declare module 'types' {
     handleClose: () => void;
   }
 
+  export interface LoginModeTogglePropsType {
+    isLoginMode: boolean;
+    loginModeHandler: () => void;
+  }
+
+  export interface LoginFormPropsType {
+    formSubmitHandler: (e: React.MouseEvent<HTMLFormElement>) => void;
+    message?: string;
+    emailRef: React.RefObject<HTMLInputElement>;
+    isLoginMode: boolean;
+  }
+
+  export interface LoginBannerPropsType {
+    isLoginMode: boolean;
+    handleClose: () => void;
+  }
+
   export interface SettingModalPropsType {
     handleClose: () => void;
   }
   export interface OptionConfigPropsType {
     type: string;
     config: { [key: string]: boolean | undefined };
+  }
+
+  type Input = {
+    value: string | undefined;
+    isValid: boolean;
+  };
+
+  export interface Inputs {
+    [key: string]: Input;
+  }
+
+  export interface State {
+    inputs: Inputs;
+    isValid: boolean;
+  }
+
+  export interface Action {
+    type: 'INPUT_CHANGE';
+    inputId: string;
+    value: string;
+    isValid: boolean;
+  }
+
+  export interface ResponseType {
+    message: string;
   }
 }
