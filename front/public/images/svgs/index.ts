dec6d1deb312f25d7ef81525488bba6fb808a941
brgndy <brgndy96@gmail.com>
2023-11-25 14:17:50 +0900

    Style: svg 파일들 추가

--- front/public/images/svgs/index.ts
+++ front/public/images/svgs/index.ts
@@ -2,10 +2,14 @@ import WhiteBannerLogoImage from './WhiteBanner.svg';
 import DarkBannerLogoImage from './DarkBanner.svg';
 import DarkLoginModalBannerImage from './DarkLoginModalImage.svg';
 import WhiteLoginModalBannerImage from './WhiteLoginModalImage.svg';
+import WhiteLoadingSpinnerImage from './WhiteLoadingSpinner.svg';
+import DarkLoadingSpinnerImage from './DarkLoadingSpinner.svg';
 
 export {
   WhiteBannerLogoImage,
   DarkBannerLogoImage,
   DarkLoginModalBannerImage,
   WhiteLoginModalBannerImage,
+  WhiteLoadingSpinnerImage,
+  DarkLoadingSpinnerImage,
 };
