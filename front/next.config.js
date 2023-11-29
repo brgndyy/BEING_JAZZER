f65e92205e3f5d7a476308e87d6a19f7446e6e22
brgndy <brgndy96@gmail.com>
2023-11-29 09:03:19 +0900

    Chore: config.js 코드 변경

--- front/next.config.js
+++ front/next.config.js
@@ -5,6 +5,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, options) => {
+    // WAV 파일 처리를 위한 설정
     config.module.rules.push({
       test: /\.(wav)$/,
       use: {
@@ -16,6 +17,26 @@ const nextConfig = {
         },
       },
     });
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    if (fileLoaderRule) {
+      config.module.rules.push({
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      });
+
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      });
+
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    // 나머지 설정
     return config;
   },
   images: {
