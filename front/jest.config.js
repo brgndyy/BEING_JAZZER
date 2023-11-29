856f62e12885441483c4fb7ac753438e36bf0ea7
brgndy <brgndy96@gmail.com>
2023-11-29 09:03:55 +0900

    Test: 테스트를 위한 cofig 작성 및 mock 파일 생성

--- front/jest.config.js
+++ front/jest.config.js
@@ -2,13 +2,30 @@ const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({ dir: './' });
 
+/** @type {import('jest').Config} */
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   testEnvironment: 'jest-environment-jsdom',
   transform: {
+    '\\.css\\.ts$': '@vanilla-extract/jest-transform',
     '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.svg$': 'jest-transformer-svg',
   },
 };
 
-module.exports = createJestConfig(customJestConfig);
+const jestConfigWithOverrides = async (...args) => {
+  const configFn = createJestConfig(customJestConfig);
+  const res = await configFn(...args);
+
+  res.moduleNameMapper = {
+    '\\.svg': '<rootDir>/__mocks__/svg.js',
+    ...res.moduleNameMapper,
+  };
+
+  return res;
+};
+
+module.exports = jestConfigWithOverrides;
