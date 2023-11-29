c52457083d48990b66f4b0eb7f1f3ce8ac6132d2
brgndy <brgndy96@gmail.com>
2023-11-30 07:52:06 +0900

    Refactor: eslint룰에 맞춰서 파일 재저장

--- front/src/app/register/page.tsx
+++ front/src/app/register/page.tsx
@@ -1,21 +1,21 @@
-import { SearchParamsType } from 'types'
-import getSignupUserEmail from '@/_services/getSignupUserEmail'
-import RegisterFunnel from '@/_components/_register/RegisterFunnel'
-import getEncryptedCodeFromParams from '@/_services/getEncryptedCodeFromParams'
-import NotFound from '../not-found'
+import { SearchParamsType } from 'types';
+import getSignupUserEmail from '@/_services/getSignupUserEmail';
+import RegisterFunnel from '@/_components/_register/RegisterFunnel';
+import getEncryptedCodeFromParams from '@/_services/getEncryptedCodeFromParams';
+import NotFound from '../not-found';
 
 export default async function RegisterPage({ searchParams }: SearchParamsType) {
-  const encryptedCode = getEncryptedCodeFromParams({ searchParams })
-  const userInfo = await getSignupUserEmail(encryptedCode)
-  const { userEmail } = userInfo
+  const encryptedCode = getEncryptedCodeFromParams({ searchParams });
+  const userInfo = await getSignupUserEmail(encryptedCode);
+  const { userEmail } = userInfo;
 
   if (!userEmail) {
-    return <NotFound />
+    return <NotFound />;
   }
 
   return (
     <>
       <RegisterFunnel userEmail={userEmail} />;
     </>
-  )
+  );
 }
