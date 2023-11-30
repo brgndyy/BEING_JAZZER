e61e012f331f9eecc533848322b188e374bd899f
brgndy <brgndy96@gmail.com>
2023-11-30 13:15:56 +0900

    Feat(constants): 에러 상수 메세지 추가

--- backend/src/constants/errorMessages.ts
+++ backend/src/constants/errorMessages.ts
@@ -6,6 +6,7 @@ const ERROR_MESSAGES = deepFreeze({
   not_found_user: '회원정보 조회에 실패했어요!',
   fail_send_email: '이메일을 보내는데에 실패했어요!',
   not_found_email_record: '이메일 인증 기록이 존재하질 않아요!',
+  existing_user: '이미 존재하는 회원이에요!',
 });
 
 export default ERROR_MESSAGES;
