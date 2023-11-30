a0ff48a814064873e829ff7d980deeb1c9c5a442
brgndy <brgndy96@gmail.com>
2023-11-30 13:11:37 +0900

    Feat(funnel): 총체적인 퍼널 컴포넌트 코드 작성

--- front/src/_components/_register/RegisterFunnel.tsx
+++ front/src/_components/_register/RegisterFunnel.tsx
@@ -3,53 +3,124 @@
 import { RegisterFromPropsType } from 'types';
 import useFunnel from '@/_hooks/useFunnel';
 import { AnimatePresence, motion } from 'framer-motion';
+import { myStyle } from '@/_styles/vars.css';
+import useForm from '@/_hooks/useForm';
+import useFetch from '@/_hooks/useFetch';
+import { Suspense } from 'react';
+import PATH_ROUTES from '@/_constants/pathRoutes';
 import FunnelCard from '../_composables/cards/FunnelCard';
 import WelcomeFunnel from './WelcomeFunnel';
 import UserNameFunnel from './UserNameFunnel';
 import UserImageFunnel from './UserImageFunnel';
 import FinishFunnel from './FinishFunnel';
+import { funnelConatiner } from './funnel.css';
+import UserEmailFunnel from './UserEmailFunnel';
+import LoadingSpinner from '../_composables/loadingSpinner/LoadingSpinner';
+
+const boxVariants = {
+  entry: (direction: 'next' | 'previous') => ({
+    x: direction === 'previous' ? '-500' : '500',
+    opacity: 0,
+    scale: 0,
+  }),
+  center: {
+    opacity: 1,
+    x: 0,
+    scale: 1,
+    transition: { duration: 0.5 },
+  },
+  exit: (direction: 'next' | 'previous') => ({
+    x: direction === 'previous' ? '500' : '-500',
+    opacity: 0,
+    scale: 0,
+    transition: { duration: 0.5 },
+  }),
+};
 
 export default function RegisterFunnel({ userEmail }: RegisterFromPropsType) {
-  const { step, Funnel, nextStepHandler, previousStepHandler, animationDirection } = useFunnel([
+  const { step, Funnel, nextStepHandler, previousStepHandler, direction } = useFunnel([
     'welcome',
+    'userEmail',
     'userName',
     'userImage',
     'finish',
   ] as const);
 
+  const { formState, inputHandler, inputFileUploadHandler } = useForm({
+    userName: '',
+    userEmail,
+    userImage: null,
+  });
+
+  const { sendRequest } = useFetch();
+
+  const formSubmitHandler = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const res = await sendRequest(
+      `${process.env.NEXT_PUBLIC_DEFAULT_BE_URL}${PATH_ROUTES.signup}`,
+      JSON.stringify({
+        userName: typeof formState.userName === 'string' ? formState.userName : '',
+        userEmail,
+        userImage: formState.userImage,
+      }),
+      {
+        'Content-Type': 'application/json',
+      },
+      'POST',
+    );
+
+    const data = await res.json();
+
+    console.log(data);
+  };
+
   return (
-    <FunnelCard>
-      <AnimatePresence initial={false}>
-        <motion.div
-          key={step}
-          initial={
-            animationDirection === 'next' ? { x: '100%', opacity: 0 } : { x: '-100%', opacity: 0 }
-          }
-          animate={{ x: 0, opacity: 1 }}
-          exit={
-            animationDirection === 'next' ? { x: '-200%', opacity: 0 } : { x: '200%', opacity: 0 }
-          }
-          transition={{ duration: 0.3 }}
-        >
-          <Funnel>
-            <Funnel.Step name="welcome">
-              <WelcomeFunnel nextStepHandler={nextStepHandler} />
-            </Funnel.Step>
-            <Funnel.Step name="userName">
-              <UserNameFunnel
-                previoustStepHandler={previousStepHandler}
-                nextStepHandler={nextStepHandler}
-              />
-            </Funnel.Step>
-            <Funnel.Step name="userImage">
-              <UserImageFunnel previoustStepHandler={previousStepHandler} />
-            </Funnel.Step>
-            <Funnel.Step name="finish">
-              <FinishFunnel />
-            </Funnel.Step>
-          </Funnel>
-        </motion.div>
-      </AnimatePresence>
-    </FunnelCard>
+    <Suspense fallback={<LoadingSpinner />}>
+      <FunnelCard>
+        <AnimatePresence custom={direction} initial={false}>
+          <motion.div
+            className={`${funnelConatiner} ${myStyle}`}
+            key={step}
+            variants={boxVariants}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            custom={direction}
+          >
+            <Funnel step={step}>
+              <Funnel.Step name="welcome">
+                <WelcomeFunnel nextStepHandler={nextStepHandler} />
+              </Funnel.Step>
+              <Funnel.Step name="userEmail">
+                <UserEmailFunnel
+                  userEmail={userEmail}
+                  nextStepHandler={nextStepHandler}
+                  previoustStepHandler={previousStepHandler}
+                />
+              </Funnel.Step>
+              <Funnel.Step name="userName">
+                <UserNameFunnel
+                  previoustStepHandler={previousStepHandler}
+                  nextStepHandler={nextStepHandler}
+                  value={typeof formState.userName === 'string' ? formState.userName : ''}
+                  inputHandler={inputHandler}
+                />
+              </Funnel.Step>
+              <Funnel.Step name="userImage">
+                <UserImageFunnel
+                  previoustStepHandler={previousStepHandler}
+                  inputFileUploadHandler={inputFileUploadHandler}
+                  formSubmitHandler={formSubmitHandler}
+                />
+              </Funnel.Step>
+              <Funnel.Step name="finish">
+                <FinishFunnel />
+              </Funnel.Step>
+            </Funnel>
+          </motion.div>
+        </AnimatePresence>
+      </FunnelCard>
+    </Suspense>
   );
 }
