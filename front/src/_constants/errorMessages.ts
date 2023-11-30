1b1cbb29eefad05d9bf55704fcfb5f60b3adbdb8
brgndy <brgndy96@gmail.com>
2023-11-30 13:13:54 +0900

    Feat(constants): 에러메세지, 경로 상수 추가

--- front/src/_constants/errorMessages.ts
+++ front/src/_constants/errorMessages.ts
@@ -2,6 +2,7 @@ import deepFreeze from '@/_utils/deepFreeze';
 
 const ERROR_MESSAGES = deepFreeze({
   fail_send_email: '이메일 전송에 실패했어요!',
+  invalid_name: '이름은 1글자 이상의 필수 입력값이에요!',
 });
 
 export default ERROR_MESSAGES;
--- front/src/_constants/pathRoutes.ts
+++ front/src/_constants/pathRoutes.ts
@@ -3,6 +3,7 @@ import deepFreeze from '@/_utils/deepFreeze';
 const PATH_ROUTES = deepFreeze({
   request_auth_email: '/api/users/sendAuthEmail',
   register_user_info: '/api/users/register',
+  signup: '/api/users/signup',
 });
 
 export default PATH_ROUTES;
