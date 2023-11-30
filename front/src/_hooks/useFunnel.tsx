e27f2d5f32e30533ce4a2320b1e550d4c5d6120e
brgndy <brgndy96@gmail.com>
2023-11-30 10:27:05 +0900

    Refactor(custom hook): useFunnel 코드 수정

--- front/src/_hooks/useFunnel.tsx
+++ front/src/_hooks/useFunnel.tsx
@@ -5,39 +5,39 @@ interface StepProps {
   children: ReactNode;
 }
 
+function Funnel({ children, step }: { children: ReactNode; step: string }) {
+  const validElements = Children.toArray(children).filter(
+    (child) => isValidElement(child) && child.props.name === step,
+  );
+
+  return <>{validElements}</>;
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Step({ name, children }: StepProps) {
   return <>{children}</>;
 }
 
+Funnel.Step = Step;
+
 const useFunnel = (steps: readonly string[]) => {
   const [step, setStep] = useState(steps[0]);
   const [stepIndex, setStepIndex] = useState<number>(0);
-  const [animationDirection, setAnimationDirection] = useState<'next' | 'previous'>('next');
-
-  function Funnel({ children }: { children: ReactNode }) {
-    const validElements = Children.toArray(children).filter(
-      (child) => isValidElement(child) && child.props.name === step,
-    );
-
-    return <>{validElements}</>;
-  }
-
-  Funnel.Step = Step;
+  const [direction, setDirection] = useState<'next' | 'previous'>('next');
 
   const nextStepHandler = () => {
-    setAnimationDirection('next');
+    setDirection('next');
     setStepIndex((prevStepIndex) => prevStepIndex + 1);
     setStep(steps[stepIndex + 1]);
   };
 
   const previousStepHandler = () => {
-    setAnimationDirection('previous');
+    setDirection('previous');
     setStepIndex((prevStepIndex) => prevStepIndex - 1);
     setStep(steps[stepIndex - 1]);
   };
 
-  return { step, Funnel, nextStepHandler, previousStepHandler, animationDirection };
+  return { step, Funnel, nextStepHandler, previousStepHandler, direction };
 };
 
 export default useFunnel;
