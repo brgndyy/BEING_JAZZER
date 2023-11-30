afca17d529338be88146ad426403d8533b90389a
brgndy <brgndy96@gmail.com>
2023-11-30 13:14:26 +0900

    Feat(route): Express 라우트 추가

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { sendLoginOrSignUpEmail } from '../controllers/sendLoginOrSignupEmail';
+import sendLoginOrSignUpEmail from '../controllers/sendLoginOrSignupEmail';
 import decryptAndRetrieveEmail from '../controllers/decryptAndRetrieveEmail';
+import userSignUp from '../controllers/signUpUser';
+import userProfileImageUpload from '../utils/multerStorage/userProfileImageStorage';
 
 const router = Router();
 
 router.post('/sendAuthEmail', sendLoginOrSignUpEmail);
 router.post('/register', decryptAndRetrieveEmail);
+router.post('/signup', userProfileImageUpload.single('image'), userSignUp);
 
 export { router as userRoutes };
