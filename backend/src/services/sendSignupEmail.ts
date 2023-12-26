e7474abe87b2422f3d16f2a037e09cc0745c9d54
brgndy <brgndy96@gmail.com>
2023-12-27 08:14:10 +0900

    Feat: 회원가입 메일 보내기 기능구현

---
+++ backend/src/services/sendSignupEmail.ts
@@ -0,0 +1,34 @@
+import { AuthEmailRecord } from '../models/authEmailRecords';
+import { encryptEmail } from '../utils/encryptEmail';
+import transporter from './mailService/transporter';
+import HttpError from '../error/HttpError';
+import { renderHtml } from './mailService/renderHtml';
+import ERROR_MESSAGES from '../constants/errorMessages';
+import createMailOption from './mailService/createMailOption';
+import { MAIL_STATE_OPTIONS } from '../constants/options';
+
+const sendSignupEmail = async (userEmail: string) => {
+  try {
+    // 만약에 이미 인증 링크를 받은 사용자가 또 이메일 인증 메일을 받는다면
+    // db에서 삭제 후 갱신
+    await AuthEmailRecord.destroy({ where: { userEmail: userEmail } });
+
+    const encryptedCode = await encryptEmail(userEmail);
+
+    await AuthEmailRecord.create({
+      userEmail: userEmail,
+      encryptedCode: encryptedCode,
+    });
+
+    const html = renderHtml('회원가입', encryptedCode);
+
+    const mailOption = createMailOption(userEmail, MAIL_STATE_OPTIONS.sign_up, html);
+
+    await transporter.sendMail(mailOption);
+  } catch (err) {
+    console.error(err);
+    throw new HttpError(ERROR_MESSAGES.fail_send_email, 503);
+  }
+};
+
+export default sendSignupEmail;
