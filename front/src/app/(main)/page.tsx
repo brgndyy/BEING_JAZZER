39d38a4886df067172dd68be6310a8dca4ba7026
brgndy <brgndy96@gmail.com>
2024-01-01 08:26:34 +0900

    Feat: 페이지 컴포넌트 생성

--- front/src/app/(main)/page.tsx
+++ front/src/app/(main)/page.tsx
@@ -1,3 +1,5 @@
+import PlayOfModeLink from '@/_components/_composables/links/PlayOfModeLink';
+
 export default async function MainPage() {
-  return <div>main page</div>;
+  return <PlayOfModeLink />;
 }
--- front/src/app/(main)/play/chords/page.tsx
+++ front/src/app/(main)/play/chords/page.tsx
@@ -1,3 +1,14 @@
+import getAccessTokenValue from '@/_services/token/getAccessTokenValue';
+import getUserInfoByAccessToken from '@/_services/auth/getUserInfoByAccessToken';
+import getUserChordImage from '@/_services/images/getUserChordImage';
+import getDefaultChordImage from '@/_services/images/getDefaultChordImage';
+import PageOfSingleChord from '@/_components/_play/PageOfSingleChord';
+
 export default async function PlayingChordPage() {
-  return <div>play page</div>;
+  const accessToken = getAccessTokenValue();
+  const userInfo = await getUserInfoByAccessToken(accessToken);
+  const chordImages =
+    accessToken && userInfo ? await getUserChordImage(accessToken) : await getDefaultChordImage();
+
+  return <PageOfSingleChord />;
 }
