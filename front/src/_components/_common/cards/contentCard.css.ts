dfab46bd3307da5ff7077cded5082410ff79f466
brgndy <brgndy96@gmail.com>
2024-07-03 21:27:31 +0900

    design: 레이아웃 컴포넌트 높이 수정

--- front/src/_components/_common/cards/contentCard.css.ts
+++ front/src/_components/_common/cards/contentCard.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const contentCard = style({
   width: '100%',
   height: 'auto',
-  minHeight: '100%',
+  minHeight: '100vh',
   paddingTop: '5rem',
   paddingBottom: '4rem',
 });
