dab75e5ea97538a09c056e3f33f656fd2e78e419
brgndy <brgndy96@gmail.com>
2024-07-14 12:54:40 +0900

    test: useCachedMutation 훅에 대한 테스트 코드 작성

---
+++ front/src/_hooks/__tests__/useCachedMutation.test.tsx
@@ -0,0 +1,69 @@
+import { renderHook, act } from '@testing-library/react';
+import { useCachedMutation } from '../mutations/useCachedMutate';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
+
+const createQueryClient = () => new QueryClient();
+
+const wrapper = ({ children }: PropsWithChildren) => (
+  <QueryClientProvider client={createQueryClient()}>{children}</QueryClientProvider>
+);
+
+const mockMutationFn = jest.fn();
+const initialVariables = { test: 'initial' };
+
+describe('useCachedMutation 훅에 대한 테스트', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('초기 값이 정상적으로 설정 된다.', () => {
+    const { result } = renderHook(
+      () => useCachedMutation(mockMutationFn, { initialValue: initialVariables }),
+      { wrapper },
+    );
+
+    expect(result.current.mutate).toBeDefined();
+  });
+
+  it('만약 인자로 넣어주는 값이 같다면 mutate 함수를 호출하지 않는다.', async () => {
+    const { result } = renderHook(
+      () => useCachedMutation(mockMutationFn, { initialValue: initialVariables }),
+      { wrapper },
+    );
+
+    await act(async () => {
+      result.current.mutate(initialVariables);
+    });
+
+    expect(mockMutationFn).not.toHaveBeenCalled();
+  });
+
+  it('만약 인자로 넣어주는 값이 다르다면 mutate 함수를 호출한다.', async () => {
+    const { result } = renderHook(
+      () => useCachedMutation(mockMutationFn, { initialValue: initialVariables }),
+      { wrapper },
+    );
+
+    const newVariables = { test: 'new' };
+    await act(async () => {
+      result.current.mutate(newVariables);
+    });
+
+    expect(mockMutationFn).toHaveBeenCalledWith(newVariables);
+  });
+
+  it('만약 인자로 넣어주는 값이 같다면 onSettled 함수를 호출한다.', async () => {
+    const onSettled = jest.fn();
+    const { result } = renderHook(
+      () => useCachedMutation(mockMutationFn, { initialValue: initialVariables, onSettled }),
+      { wrapper },
+    );
+
+    await act(async () => {
+      result.current.mutate(initialVariables);
+    });
+
+    expect(onSettled).toHaveBeenCalled();
+  });
+});
