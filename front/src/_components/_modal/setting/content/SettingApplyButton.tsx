8a18ea77ac3248da53d951e8ee5fdd98dec5d661
brgndy <brgndy96@gmail.com>
2024-07-16 20:02:54 +0900

    feat: 코드 셋팅 관련 최소 설정 제한

--- front/src/_components/_modal/setting/content/SettingApplyButton.tsx
+++ front/src/_components/_modal/setting/content/SettingApplyButton.tsx
@@ -5,32 +5,55 @@ import useChangeUserChordSetting from '@/_hooks/mutations/useChangeUserChordSett
 import useAccessTokenStore from '@/_store/accessTokenStore';
 import { toast } from 'react-toastify';
 import LoadingSpinner from '@/_components/_common/loadingSpinner/LoadingSpinner';
+import { ChordSetting, ChordSettingKey } from '@/_types';
+import ERROR_MESSAGES from '@/_constants/errorMessages';
+import useChangeDefaultChordSetting from '@/_hooks/mutations/useChangeDefaultChordSetting';
 
 type Props = {
   handleClose: () => void;
 };
 
+const isValidSetting = (chordSetting: ChordSetting[], type: ChordSettingKey) => {
+  return chordSetting.some(
+    (setting) =>
+      setting.type === type &&
+      Object.values(setting.config).some((config) => config.isAvailable && config.isSelected),
+  );
+};
+
+const validateMinimumSetting = (chordSetting: ChordSetting[]) => {
+  const requiredTypes: ChordSettingKey[] = ['Key', 'Chord', 'Tension'];
+  return requiredTypes.every((type) => isValidSetting(chordSetting, type));
+};
+
 export default function SettingApplyButton({ handleClose }: Props) {
   const { chordSetting } = useSelectSettingOption();
   const { accessToken } = useAccessTokenStore();
-  const { handleUserChordSetting, isPending } = useChangeUserChordSetting({
-    handleClose,
-    initialChordSetting: chordSetting,
-    accessToken,
-  });
+  const isCompletedMinimumSetting = validateMinimumSetting(chordSetting);
+  const { handleUserChordSetting, isPending: isUserChordChangePending } = useChangeUserChordSetting(
+    {
+      handleClose,
+      initialChordSetting: chordSetting,
+      accessToken,
+    },
+  );
 
   const handleTotalUserChordSetting = () => {
-    if (accessToken) {
-      handleUserChordSetting({ accessToken, chordSetting });
+    if (isCompletedMinimumSetting) {
+      if (accessToken) {
+        handleUserChordSetting({ accessToken, chordSetting });
+      } else {
+        // TODO 비회원일때 코드 설정을 변경하면 발생할 로직 설정
+        toast.error('에러가 발생했어요!');
+      }
     } else {
-      // TODO 비회원일때 코드 설정을 변경하면 발생할 로직 설정
-      toast.error('에러가 발생했어요!');
+      toast.error(ERROR_MESSAGES.not_satisfied_chord_setting);
     }
   };
 
   return (
     <>
-      {isPending && <LoadingSpinner />}
+      {isUserChordChangePending && <LoadingSpinner />}
       <div className={buttonContainer}>
         <button
           onClick={handleTotalUserChordSetting}
