6f46967a2d97db5d4c22572e2994ce0c45b5761a
brgndy <brgndy96@gmail.com>
2024-07-19 07:14:16 +0900

    chore: 이미지 경로 패턴 추가

--- front/next.config.js
+++ front/next.config.js
@@ -42,11 +42,11 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'http',
-      //   hostname: 'localhost',
-      //   port: '3002',
-      // },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3002',
+      },
       {
         protocol: 'https',
         hostname: 'd2qtp7qksp5k9g.cloudfront.net',
