1f9bda615eca27bd80992ba687c3ba59984404c6
brgndy <brgndy96@gmail.com>
2024-08-13 21:52:29 +0900

    test: repush

--- front/src/_apis/clients/beingJazzerClient.ts
+++ front/src/_apis/clients/beingJazzerClient.ts
@@ -5,9 +5,8 @@ export const baseUrl =
   process.env.NEXT_PUBLIC_FRONT_ENV_MODE === 'production' ? BASE_URL.prod : BASE_URL.dev;
 // export const baseUrl = BASE_URL.prod;
 
-const beingJazzerClient = new APIClient(baseUrl);
-
 console.log('baseUrl : ', baseUrl);
+const beingJazzerClient = new APIClient(baseUrl);
 
 beingJazzerClient.setErrorHandler((error) => {
   console.error('Custom API error handler:', error);
