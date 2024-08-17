12b6b77c6e25b50562768a44d90f062ea4000049
brgndy <brgndy96@gmail.com>
2024-08-17 23:25:38 +0900

    fix: 잘못된 코드 삭제

--- front/src/app/(main)/page.tsx
+++ front/src/app/(main)/page.tsx
@@ -1,8 +1,5 @@
 import PlayOfModeLink from '@/_components/_common/links/PlayOfModeLink';
-import { cookies } from 'next/headers';
 
 export default async function MainPage() {
-  cookies().set('test', 'test');
-
   return <PlayOfModeLink />;
 }
