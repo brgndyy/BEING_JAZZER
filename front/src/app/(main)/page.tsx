76cdc60e6c2945d1dc41a40a8bd1521422861f48
brgndy <brgndy96@gmail.com>
2024-08-17 23:17:23 +0900

    chore: sentry 적용

--- front/src/app/(main)/page.tsx
+++ front/src/app/(main)/page.tsx
@@ -1,5 +1,8 @@
 import PlayOfModeLink from '@/_components/_common/links/PlayOfModeLink';
+import { cookies } from 'next/headers';
 
 export default async function MainPage() {
+  cookies().set('test', 'test');
+
   return <PlayOfModeLink />;
 }
--- front/src/app/global-error.tsx
+++ front/src/app/global-error.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import * as Sentry from '@sentry/nextjs';
+import Error from 'next/error';
+import { useEffect } from 'react';
+
 export default function GlobalError({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   error,
@@ -8,6 +12,10 @@ export default function GlobalError({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
   return (
     <html lang="en">
       <body>
