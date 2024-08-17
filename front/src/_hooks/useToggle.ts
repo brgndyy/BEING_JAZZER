cbc94b025bf5f42016475fb6e3cc862c1a0c807b
JEON TAEHEON <brgndyy@gmail.com>
2024-08-17 14:06:52 +0900

    탭 바깥쪽을 클릭해도 닫히는 기능 구현 (issue #45) (#48)
    * refactor: useSingleRequestMutation 리팩토링
    
    * feat: useOutsideClick 훅 생성 및 테스트 코드 작성
    
    * refactor: 메세지 상수처리

---
+++ front/src/_hooks/useToggle.ts
@@ -0,0 +1,21 @@
+import { useState, useCallback } from 'react';
+
+const useToggle = (initialState = false) => {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const toggle = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, []);
+
+  const open = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const close = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  return { isOpen, toggle, open, close };
+};
+
+export default useToggle;
