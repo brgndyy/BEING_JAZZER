808ffacf382ea46f93ee1a676145f969c13a62ee
JEON TAEHEON <brgndyy@gmail.com>
2024-08-19 20:33:30 +0900

    refactor: 백엔드 경로 수정 및 메일 템플릿 스타일링 (#66)

--- backend/src/services/mailService/renderHtml.ts
+++ backend/src/services/mailService/renderHtml.ts
@@ -2,11 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import nunjucks from 'nunjucks';
 import PATH from '../../constants/path';
+import BASE_URL from '../../constants/baseUrl';
 
 const htmlString = fs.readFileSync(path.join(process.cwd(), PATH.auth_html_file), 'utf-8');
 
 export const renderHtml = (state: string, encryptedCode: string) =>
   nunjucks.renderString(htmlString, {
+    BASE_URL: BASE_URL,
     loginState: state,
     encryptedCode: encryptedCode,
   });
